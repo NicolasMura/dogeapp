ddc4d237fd9f1a05a9ee53f6634d2dfb96ca31af
NicolasMura <contact@nicolasmura.fr>
2026-02-16 13:22:55 +0100

    test(market): add unit test for MarketPage reload functionality

--- src/app/features/market/pages/market-page/market-page.spec.ts
+++ src/app/features/market/pages/market-page/market-page.spec.ts
@@ -7,6 +7,18 @@ describe('MarketPage', () => {
   it('should create the MarketPage', () => {
     const fixture = MockRender(MarketPage);
     const component = fixture.point.componentInstance;
+
     expect(component).toBeTruthy();
+  });
+
+  it('reload should call refresh', () => {
+    const fixture = MockRender(MarketPage);
+    const component = fixture.point.componentInstance;
+
+    vi.spyOn(component.refresh$, 'next');
+
+    component.reload();
+
+    expect(component.refresh$.next).toHaveBeenCalled();
   });
 });

92eb8c32b620607a9e5dbddd2efb61039776080f
NicolasMura <contact@nicolasmura.fr>
2026-02-18 21:38:41 +0100

    refactor(app): remove unused signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '@doge/ui';
 
@@ -8,6 +8,4 @@ import { Header } from '@doge/ui';
   templateUrl: './app.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {
-  protected readonly title = signal('dogeapp');
-}
+export class App {}
